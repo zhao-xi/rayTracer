776b0802061f076653947875b3abf5e3cc9a315b
DESKTOP-HHBA55H\zhaox <zhaoxi0418@outlook.com>
2018-04-29 00:51:26 +0900

    no message

--- rayTracer/camera.h
+++ rayTracer/camera.h
@@ -5,8 +5,9 @@ class Camera {
 public:
 	Camera() {
 		lower_left_corner = vec3(-2.0, -1.0, -1.0);
-		horizontal = vec3(0.0, 2.0, 0.0);
-		vertical = vec3(0.0, 0.0, 0.0);
+		horizontal = vec3(4.0, 0.0, 0.0);
+		vertical = vec3(0.0, 2.0, 0.0);
+		origin = vec3(0.0, 0.0, 0.0);
 	}
 	Ray get_ray(float u, float v) {
 		return Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
--- rayTracer/test.cpp
+++ rayTracer/test.cpp
@@ -7,15 +7,24 @@
 #include "hitableList.h"
 #include "camera.h"
 using namespace std;
-
-vec3 color(Ray& r,hitable *world) {
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0*vec3(float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, float(rand()) / RAND_MAX) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
+vec3 color(const Ray& r, hitable *world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	if (world->hit(r, 0.001, FLT_MAX, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5*color(Ray(rec.p, target - rec.p), world);
 	}
-	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	else {
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5*(unit_direction.y() + 1.0);
+		return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	}
 }
 
 int main() {
@@ -37,13 +46,14 @@ int main() {
 		for (int i = 0; i < nx; i++) {
 			vec3 col(0.0, 0.0, 0.0);
 			for (int s = 0; s < ns; s++) {
-				float u = float(i + rand()/RAND_MAX) / float(nx);
-				float v = float(j + rand()/RAND_MAX) / float(ny);
+				float u = float(i + float(rand())/RAND_MAX) / float(nx);
+				float v = float(j + float(rand())/RAND_MAX) / float(ny);
 				Ray r = cam.get_ray(u, v);
 				vec3 p = r.point_at_parameter(2.0);
 				col += color(r, world);
 			}
 			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			float ir = int(col[0] * 255.99);
 			float ig = int(col[1] * 255.99);
 			float ib = int(col[2] * 255.99);
